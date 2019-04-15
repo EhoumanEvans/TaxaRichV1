7d4a2ce59ee933258db0429a9b59abd272d4371e
Ehouman Evans <36210209+EhoumanEvans@users.noreply.github.com>
2019-04-15 19:29:47 +0000

    Update nbgr.R
    adding header

--- R/nbgr.R
+++ R/nbgr.R
@@ -1,3 +1,15 @@
+#' This function is allows you to calculate the number of species per genra in a floristic data table
+#'
+#' @param x data matrix of floristic inventory, the name of each colum is as follow : 'genera', 'epit.spec', 'specie', 'family', 'life.form', 'chorology'.
+#' @keywords family 
+#' @author Evans Ehouman
+#' @return to a data.frame of number of species in each genera
+#' @export
+#' @examples
+#' # Find the number of species in the genera of the sample dataframe
+#' nbfam(releveforet)
+#'
+
 nbgr= function(x) {
   if (!requireNamespace("dplyr", quietly = TRUE)) {
     stop("Package \"dplyr\" needed for this function to work. Please install it.",
