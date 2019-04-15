b8db85aa17bd4f1f4a61e28edd5106c05e1d18d6
Ehouman Evans <36210209+EhoumanEvans@users.noreply.github.com>
2019-04-15 19:26:32 +0000

    Update nbchly.R
    adding the header comment

--- R/nbchly.R
+++ R/nbchly.R
@@ -1,3 +1,17 @@
+#' This function is allows you to calculate the number of species per family in a floristic data table
+#'
+#' This function converts input temperatures in Fahrenheit to Kelvin.
+#' @param x data matrix of floristic inventory, the name of each colum is as follow : 'genera', 'epit.spec', 'specie', 'family', 'life.form', 'chorology'.
+#' @keywords family 
+#' @author Evans Ehouman
+#' @return to a data.frame of number of species in each family.
+#' @export
+#' @examples
+#' # Find the number of species in the family of the sample dataframe
+#' nbfam(releveforet)
+#' 
+
+
 nbchly= function(x) {
   if (!requireNamespace("dplyr", quietly = TRUE)) {
     stop("Package \"dplyr\" needed for this function to work. Please install it.",
