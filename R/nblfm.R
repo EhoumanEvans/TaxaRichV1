e6f0902452dff3cdea202a36d1c4225fd88681bd
Ehouman Evans <36210209+EhoumanEvans@users.noreply.github.com>
2019-04-15 19:31:30 +0000

    Update nblfm.R
    adding header

--- R/nblfm.R
+++ R/nblfm.R
@@ -1,3 +1,15 @@
+#' This function is allows you to calculate the number of species per life form in a floristic data table
+#'
+#' @param x data matrix of floristic inventory, the name of each colum is as follow : 'genera', 'epit.spec', 'specie', 'family', 'life.form', 'chorology'.
+#' @keywords family 
+#' @author Evans Ehouman
+#' @return to a data.frame of number of species for each life form
+#' @export
+#' @examples
+#' # Find the number of species in the genera of the sample dataframe
+#' nblfm(releveforet)
+#'
+
 nblfm= function(x) {
   if (!requireNamespace("dplyr", quietly = TRUE)) {
     stop("Package \"dplyr\" needed for this function to work. Please install it.",
